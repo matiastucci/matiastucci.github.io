1366d1c84bd4bf8237f93121305003aad6c9631b
Matias <matiastucci@gmail.com>
2016-02-05 07:08:11 +0100

    remove unused functions

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@
     }
 
     $(window).scroll(function() {
-      
+
       if($('section:first').is('.parallax, #home, .splash')){
         if ($(window).scrollTop() >= 200 ) {
           $('#topnav').addClass('scroll');
@@ -29,69 +29,16 @@
     $('.navbar-toggle').on('click', function(event) {
       $(this).toggleClass('open');
       $('#navigation').slideToggle(400);
-      $('.cart, .search').removeClass('open');
     });
 
     $('.navigation-menu>li').slice(-2).addClass('last-elements');
 
   }
 
-  function initCarousels () {
-    $('.owl-carousel').each(function(index, el) {
-      var dataOptions = $(this).data('options') || {};
-
-      var options = {
-        items: dataOptions.items || 4,
-        loop: dataOptions.loop || true,
-        dots: dataOptions.dots || false,
-        margin: dataOptions.margin || 10,
-        autoplay: dataOptions.autoplay || false,
-        responsiveClass: true,
-        responsive:{
-          0:{
-            items: dataOptions.xsItems || 1,
-            margin: 25
-          },
-          768:{
-            items: dataOptions.smItems || 2,
-          },
-          992:{
-            items:dataOptions.mdItems || 3,
-          },
-          1200: {
-            items: dataOptions.items || 4
-          }
-        }
-      };
-
-      if (options.autoplay) {
-        options.autoplayTimeout = dataOptions.autoplayTimeout || 2000;
-        options.autoplayHoverPause = true;
-      }
-
-
-      $(el).owlCarousel(options);
-    });
-  }
-
   function initLoad () {
     $(window).load(function() {
-
       $("#loader").delay(500).fadeOut();
       $("#mask").delay(1000).fadeOut("slow");
-
-      var $grid = $('#works').isotope({
-        masonry: {
-         columnWidth: 0
-        },
-        itemSelector: '.work-item'
-      });
-
-      $grid.on('layoutComplete', function(event) {
-        $(window).trigger('resize');
-        fixScroll();
-      });
-
     });
   }
 
@@ -140,7 +87,7 @@
           var errorTemplate = '<div role="alert" class="alert alert-danger alert-outline">'+ message +'</div>';
           $('#contact-form .alert').fadeOut(300);
           $(errorTemplate).insertBefore($('#contact-form button'));
-        });    
+        });
       }
 
     });
@@ -151,11 +98,6 @@
         $(this).parent('.form-group').removeClass('has-error');
       }
     });
-  }
-
-  function fixScroll() {
-    $('#sscr').css('height', 0);
-    $('#sscr').css('height', document.documentElement.scrollHeight + 'px');
   }
 
   function initGeneral () {
@@ -184,97 +126,82 @@
       });
     }
 
-    $('.bg-img, .thumb-placeholder').each(function(index, el) {
-      var image = $(el).attr('src');
-      $(el).parent().css('background-image', 'url(' + image + ')');
-      $(el).remove();
-    });
-
-    $('.alert').on('closed.bs.alert', function () {
-      fixScroll();
-    });
-
-    $('body').on('click', '.alert', function() {
-      $(this).on('closed.bs.alert', function() {
-        fixScroll();
-      });
-    });
-
-    var parallaxZIndex = -100;
-
-    if (navigator.userAgent.indexOf("Firefox") != -1 || navigator.userAgent.indexOf("MSIE ") != -1 || navigator.userAgent.match(/Trident.*rv\:11\./) !== null){
-      parallaxZIndex = 11;
-      $('section.parallax').css('z-index', 20);
-    }
-
-    $('.parallax-bg').parallax({
-      speed: 0.5,
-      zIndex: parallaxZIndex
-    });
-
-    $('#login-tabs a:first').tab('show');
-
-    $('#login-content .tab-pane:first').addClass('fade in');
-
-    $('#login-tabs li a').on('click', function (e) {
-      e.preventDefault();
-      $(this).tab('show');
-    });
-
-    $('a[data-toggle=tab]').on('click', function(event) {
-      $(window).trigger('resize');      
-    }).on('shown.bs.tab', function(e) {
-      var container = $($(this).attr('href'));
-
-      if (container.find('.progress-bar').length) {
-        container.find('.progress-bar').each(function(index, el) {
-          $(el).css('width', $(this).data('progress') + '%');
-          $(el).parents('.skill').find('.skill-perc').css('right', 100 - $(el).data('progress') + '%');
-        });
-      }
-
-    });
-
     $('.particles-bg').particleground({
       dotColor: '#EF2D56',
       particleRadius: 5
     });
 
-    $('.boxes [data-bg-color]').each(function(index, el) {
-      $(el).css('background-color', $(el).data('bg-color'));  
-    });
+  }
 
-    $('.progress-bar').appear(function() {
-      $(this).css('width', $(this).data('progress') + '%');
-      $(this).parents('.skill').find('.skill-perc').css('right', 100 - $(this).data('progress') + '%');
-    });
-
-    $('[data-animated=true]').addClass('invisible');
-
-    $('[data-animated=true]').appear(function(){
-      var el = $(this);
-      if (el.data('delay')) {
-        setTimeout(function(){
-          el.removeClass('invisible').addClass('fade-in-top');
-        }, parseInt(el.data('delay')));
-      } else{
-        $(this).removeClass('invisible').addClass('fade-in-top');
-      }
-    }, {accX: 0, accY: 0});
-
-    $('.client-image').hover(function() {
-      $(this).removeClass('fade-in-top');
-    }, function() {
-      //
-    });
+var escudo = '  DNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN\n' +
+'  NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN\n' +
+'  NNN                                    N                                    NNN\n' +
+'  NNN                                  DNDDN  Z                               NNN\n' +
+'  NNN                               NDNNN NDNNI                               NNN\n' +
+'  NNN                              INN NZ   N,                                NNN\n' +
+'  NNN                              IN~ NZ                                     NNN\n' +
+'  NNN                              IN~ NZ                                     NNN\n' +
+'  NNN                              ?N= NZ   NN+                               NNN\n' +
+'  NNN                              ?NDMNO ZNN+                                NNN\n' +
+'  NNN                                 NNNNNN                                  NNN\n' +
+'  NNN                                   ID                                    NNN\n' +
+'  NNN                                                                         NNN\n' +
+'  NNM8DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD8888DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD88D8NNN\n' +
+'  NNN88888888888D       ~8888888888888       $D88888888888D       8888888888D8NNN\n' +
+'  NNN888888888888       ~88888888888D8       $8888888888888       888888888888NNN\n' +
+'  NNN                                                                         DNN\n' +
+'  NNN                                                                         NNN\n' +
+'  NNN            ODNNNNN7                                            IDN      NNN\n' +
+'  NNN    MNDNNND DN8  8NN                                          ONNDNN     NNN\n' +
+'  NNN   NNNN, ~DDNN    77                I                      ,NDMNN  =O:   NNN\n' +
+'  NNN   NNN     INN                     888                   =NNN INN        NNN\n' +
+'  NNN   NNN     INN                   +88Z88$                 NNNN INN        NNN\n' +
+'  NNN   INNN    INN                  88D,  D88                 :DN INN        NNN\n' +
+'  NNN    ONND   INNND              OD88     Z8DD               ~NN INN        NNN\n' +
+'  NNN     INN   IDN              ,D88    8    D88              ~NN INN        NNN\n' +
+'  NNN      DDN  INN             7DDI   D8D88   :DDO            ~NN IDD        NNN\n' +
+'  NNN       NN  INN            O88   :D88 D8D~   D8D           ~NN IO         NNN\n' +
+'  NNN       NN  IN+          O88Z   8DD     888   I888         ~NN            NNN\n' +
+'  NNN       DN              8D8 ,  8DD       8D8,   DD8        ~NN            NNN\n' +
+'  NNN   DDNNNZ            $DD=   8DO    7D$   ,DDD   ,88O      ~NN      7O    NNN\n' +
+'  NNN  ?N   NNNNDDNN     DD8   ~888    8D888    88D?   D88     ~NN     D8     NNN\n' +
+'  NNN   ~NDN   ~O$     8D8?   888    888888888    DD8   +D88   ~NDDNNNNN      NNN\n' +
+'  NNN                 8D8,   D8D    8D8888888DD    O8D:   888                 NNN\n' +
+'  NNN               O88:   DD8,   $D888DD8DD888D$    D88,   88D,              NNN\n' +
+'  NNN              888   +D88    8D8888   D88888D8,   8D8$   8D8,             NNN\n' +
+'  NNN            DD8+   8D8,   DD8D            88D8D    8D8   :8D8            NNN\n' +
+'  NNN           DDD   ,DD8    D888     8NN8N    888DD   ,$D8=   DD8,          NNN\n' +
+'  NNN        ,888,   8DD    O8D88     88NN888    D8888O    888    D88         NNN\n' +
+'  NNN        888   78DD    DD88D   D8888NN888    8888888    8D8O   888,       NNN\n' +
+'  NNN     ,8D8~   D8D    8888888   88888NN888   888888888D    D8D,   888      NNN\n' +
+'  NNN     88O   ~8DO   ,88888888   88888NN888   88888888D88,   I887   888=    NNN\n' +
+'  NNN   DD8    DD8    8D88888888   88888NN888   8888888888DDO    D88    8DD,  NNN\n' +
+'  NNN  88D   $D88    88888888888   88888NN888   88888888888888    O88D , 888, NNN\n' +
+'  NNDDD8,   888    88D888888888D   8888NN8888    88888888888888D,   88D~   8D8DNN\n' +
+'  NNN88   ?DD$   ,DD8888888888888   D888NN8888    D888888888888OD,   +DDZ   88NNN\n' +
+'  NNN    D8D    D8888888888888888D   8D88D888D    D8888888888888888    D88    MNN\n' +
+'  NND  O88O   ,88888888888888888888  8888DD8       88888888888888888    OD8O  DNN\n' +
+'  NNN,8DD    88D888D88888888888888      8888       8888888888888888888    DD8=NNN\n' +
+'  NNN8D7   :D88888888888888888888      D8888D     D8888888888888888888D    :88NNN\n' +
+'  NNMD    888888888888888888888      88888888888888888888888888888888888D    DNNN\n' +
+'  ,NND=,:DD888D8888888888888888    8888888888888888888888888888888888888DD  =MND \n' +
+'   IMDNND8D888888888888888888888888888888888888888888888888888888888888888DNNNN? \n' +
+'     DNNNNNNNNNNNNNNNNNNNNNNNNNNNNNND88888D8888NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN   \n' +
+'       8NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN88888NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNO     \n' +
+'                                   8NNNNN8DNNDNM                                 \n' +
+'                                      NDNNNNNO                                   \n' +
+'                                       DNNND                                     \n' +
+'                                        DDDD                                     ';
+  function initConsole(){
+    console.log('%c' + escudo, 'color: #319c63;');
   }
 
   function init () {
     initNavbar();
-    initCarousels();
     initLoad();
     initContactForm();
     initGeneral();
+    initConsole();
   }
 
   init();
