5663213b8677d984ae02f8955d8c5c1ab579edce
Matias Tucci <matiastucci@gmail.com>
2016-05-13 01:22:47 +0200

    service worker

---
+++ sw.js
@@ -0,0 +1,15 @@
+const cacheName = 'v1::static';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(['/']).then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
